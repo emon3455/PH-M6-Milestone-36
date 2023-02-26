d461f4817f3982b88cb1ddaf3478dd79a84b2c8e
emon3455 <emon.mhk69@gmail.com>
2023-02-26 23:40:12 +0600

    method done

---
+++ js/objectMethod.js
@@ -0,0 +1,18 @@
+const student={
+    name: "Emon",
+    age:22,
+    salary:15000,
+    subject: ["Math","Physics","Chemistry"],
+    exam: function(){
+        console.log("Just Go For the exam and get A+");
+    },
+    waiver: function(percent=0){
+        this.exam();
+        return `This semester ${this.name} get ${this.percentage = percent}% waiver`;
+    }
+
+}
+
+student.exam();
+const waiver = student.waiver(5);
+console.log(waiver);
